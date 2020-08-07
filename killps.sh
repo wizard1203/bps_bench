8cade5fba6e9f943050dd7ef8454998f2e879a55
wizard1203 <956234606@qq.com>
2020-08-07 19:07:29 +0800

    a

--- killps.sh
+++ killps.sh
@@ -1,5 +1,7 @@
 ps -ef|grep byteps|grep -v grep|cut -c 9-15|xargs kill -9
 ps -ef|grep bpslaunch|grep -v grep|cut -c 9-15|xargs kill -9
+ps -ef|grep iperf|grep -v grep|cut -c 9-15|xargs kill -9
+ps -ef|grep ib_read_bw|grep -v grep|cut -c 9-15|xargs kill -9
 ps -ef|grep imagenet|grep -v grep|cut -c 9-15|xargs kill -9
 
 
